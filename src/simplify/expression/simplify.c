a9623b38394cbb60c1a4f80ce02b5e106629966b
Ian Shehadeh <IanShehadeh2020@gmail.com>
2018-04-28 09:04:25 -0400

    significantly more stable complex simplification

--- src/simplify/expression/simplify.c
+++ src/simplify/expression/simplify.c
@@ -2,82 +2,58 @@
 
 #include "simplify/expression/simplify.h"
 
-expression_t* _expression_follow_variable_chain(expression_t* root, operator_t infix_op, int* count) {
-    expression_t* left = root;
-    while (infix_op == left->operator.infix
-            && EXPRESSION_IS_OPERATOR(left)
-            && EXPRESSION_IS_VARIABLE(EXPRESSION_RIGHT(left))) {
-        left = EXPRESSION_LEFT(left);
-        ++(*count);
+/* get the operator that would could be used to collapse a chain of `op` operators
+ *
+ * @op the source op
+ * @return returns the collapse operator, or zero if `op` is not collapseable
+ */
+operator_t _operator_collapsed_equivelent(operator_t op) {
+    switch (op) {
+        case '+':
+            return '*';
+        case '*':
+            return '^';
     }
-    return left;
+
+    return 0;
 }
 
 error_t _expression_collapse_variables_recursive(expression_t* expr) {
-    if (EXPRESSION_IS_OPERATOR(expr)) {
-        operator_t infix_op = expr->operator.infix;
-        expression_t* final = NULL;
-        int count = 2;
-        if (EXPRESSION_IS_VARIABLE(expr->operator.right)) {
-            int count = 1;
-            final = _expression_follow_variable_chain(EXPRESSION_LEFT(expr),
-                                                      expr->operator.infix,
-                                                      &count);
-        } else if (EXPRESSION_IS_OPERATOR(expr->operator.right)) {
-            return _expression_collapse_variables_recursive(expr->operator.right);
-        } else {
-            return ERROR_NO_ERROR;
-        }
+    switch (expr->type) {
+        case EXPRESSION_TYPE_NUMBER:
+        case EXPRESSION_TYPE_VARIABLE:
+        case EXPRESSION_TYPE_FUNCTION:
+        case EXPRESSION_TYPE_PREFIX:
+            break;
+        case EXPRESSION_TYPE_OPERATOR:
+        {
+            error_t err = _expression_collapse_variables_recursive(EXPRESSION_LEFT(expr));
+            if (err) return err;
 
-        expression_t* num;
-        operator_t*   op;
-        if (EXPRESSION_IS_VARIABLE(final)) {
-            ++count;
-            expression_t right = *EXPRESSION_RIGHT(expr);
-            *expr->operator.left = right;
-            num = EXPRESSION_RIGHT(expr);
-            op = &expr->operator.infix;
-        } else if (EXPRESSION_IS_OPERATOR(final)) {
-            ++count;
-            expr->operator.left = EXPRESSION_LEFT(final);
-            expression_t* var = malloc(sizeof(expression_t));
-            expression_init_variable(var, expr->operator.right->variable.value,
-                                            strlen(expr->operator.right->variable.value));
-            expression_init_operator(expr->operator.right, var,
-                                        final->operator.infix,
-                                        (expression_t*)malloc(sizeof(expression_t)));
-            num = EXPRESSION_RIGHT(EXPRESSION_RIGHT(expr));
-            op = &expr->operator.infix;
-        } else {
-            expression_t* var = malloc(sizeof(expression_t));
-            expression_copy(EXPRESSION_RIGHT(expr), var);
-            expression_init_operator(expr,
-                                        malloc(sizeof(expression_t)),
-                                        infix_op,
-                                        final);
-            expression_init_operator(EXPRESSION_LEFT(expr),
-                                        var,
-                                        0,
-                                        malloc(sizeof(expression_t)));
-            op =  &EXPRESSION_LEFT(expr)->operator.infix;
-            num = EXPRESSION_RIGHT(EXPRESSION_LEFT(expr));
-        }
+            err = _expression_collapse_variables_recursive(EXPRESSION_RIGHT(expr));
+            if (err) return err;
 
-        switch (infix_op) {
-            case '+':
-                expression_init_number_si(num, count);
-                *op = '*';
-                break;
-            case '-':
-                expression_init_number_si(num, -(count - 2));
-                *op = '*';
-                break;
-            case '*':
-                expression_init_number_si(num, count);
-                *op = '^';
-                break;
-            default:
-                break;
+            if (EXPRESSION_IS_VARIABLE(EXPRESSION_LEFT(expr))
+                && EXPRESSION_IS_VARIABLE(EXPRESSION_RIGHT(expr))
+                && !strcmp(EXPRESSION_LEFT(expr)->variable.value,
+                          EXPRESSION_RIGHT(expr)->variable.value)
+                && _operator_collapsed_equivelent(expr->operator.infix)) {
+                expr->operator.infix = _operator_collapsed_equivelent(expr->operator.infix);
+                expression_clean(expr->operator.right);
+                expression_init_number_si(EXPRESSION_RIGHT(expr), 2);
+            }
+
+            if (EXPRESSION_IS_VARIABLE(EXPRESSION_RIGHT(expr))
+                && EXPRESSION_IS_OPERATOR(EXPRESSION_LEFT(expr))
+                && EXPRESSION_IS_VARIABLE(EXPRESSION_LEFT(EXPRESSION_LEFT(expr)))
+                && EXPRESSION_IS_NUMBER(EXPRESSION_RIGHT(EXPRESSION_LEFT(expr)))
+                && !strcmp(EXPRESSION_LEFT(EXPRESSION_LEFT(expr))->variable.value,
+                           EXPRESSION_RIGHT(expr)->variable.value)
+                && EXPRESSION_LEFT(expr)->operator.infix == _operator_collapsed_equivelent(expr->operator.infix)) {
+                mpfr_ptr x = EXPRESSION_RIGHT(EXPRESSION_LEFT(expr))->number.value;
+                mpfr_add_si(x, x, 1, MPFR_RNDN);
+                *expr = *EXPRESSION_LEFT(expr);
+            }
         }
     }
     return ERROR_NO_ERROR;
