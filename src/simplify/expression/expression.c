9af3d3be87b88fe76b1d6604181e433464522359
Ian Shehadeh <IanShehadeh2020@gmail.com>
2018-05-12 17:41:43 -0400

    apparently one was used

--- src/simplify/expression/expression.c
+++ src/simplify/expression/expression.c
@@ -2,6 +2,26 @@
 
 #include "simplify/expression/expression.h"
 
+variable_t _expression_find_variable_recursive(expression_t* expr) {
+    switch (expr->type) {
+        case EXPRESSION_TYPE_FUNCTION:
+        case EXPRESSION_TYPE_NUMBER:
+            return NULL;
+        case EXPRESSION_TYPE_OPERATOR:
+        {
+            variable_t var = _expression_find_variable_recursive(expr->operator.left);
+            if (var) return var;
+            var = _expression_find_variable_recursive(expr->operator.right);
+            return var;
+        }
+        case EXPRESSION_TYPE_PREFIX:
+            return _expression_find_variable_recursive(expr->operator.right);
+        case EXPRESSION_TYPE_VARIABLE:
+            return expr->variable.value;
+    }
+    return NULL;
+}
+
 
 int _expression_has_variable_or_function_recursive(expression_t* expr, variable_t var) {
     switch (expr->type) {
@@ -32,4 +52,8 @@ int _expression_has_variable_or_function_recursive(expression_t* expr, variable_
 
 int expression_has_variable_or_function(expression_t* expr, variable_t var) {
     return _expression_has_variable_or_function_recursive(expr, var);
+}
+
+variable_t expression_find_variable(expression_t* expr) {
+    return _expression_find_variable_recursive(expr);
 }
